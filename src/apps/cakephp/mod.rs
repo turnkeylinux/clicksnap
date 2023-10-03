1f0045017a1ac734ed0a5efe929df6923e7135e0
Anton <anton@turnkeylinux.org>
2023-10-03 16:22:00 +0200

    generic: add adminer runner for mysql & postgres

--- src/apps/cakephp/mod.rs
+++ src/apps/cakephp/mod.rs
@@ -1,8 +1,8 @@
+use super::generic::adminer::{self, Flavor};
 use crate::Runner;
 use crate::{Action, State};
 use async_trait::async_trait;
 use thirtyfour::prelude::*;
-use url::ParseError;
 
 pub struct T();
 
@@ -22,24 +22,7 @@ impl Runner for T {
                     .screenshot(&st.ssp.join("screenshot-docs.png"))
                     .await?;
                 // adminer
-                let mut u = st.url.clone();
-                u.set_scheme("https").map_err(|_| ParseError::InvalidPort)?; // FIXME?
-                u.set_port(Some(12322))
-                    .map_err(|_| ParseError::InvalidPort)?; // FIXME?
-                st.wd.goto(u.as_str()).await?;
-                (st.wd.find(By::Name("auth[username]")).await?)
-                    .send_keys("adminer")
-                    .await?;
-                (st.wd.find(By::Name("auth[password]")).await?)
-                    .send_keys(&st.pse.root_pass)
-                    .await?;
-                (st.wd.find(By::Css("input[type='submit']")).await?)
-                    .click()
-                    .await?;
-                st.wd
-                    .screenshot(&st.ssp.join("screenshot-adminer-frontpage.png"))
-                    .await?;
-                Ok(())
+                adminer::T(Flavor::MySQL).exec(st).await
             }
             Action::Install => {
                 // there is nothing to install
