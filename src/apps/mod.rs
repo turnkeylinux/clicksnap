7edc52adef1ea4d5d9ed11c9cc83a569eb05a8d8
Anton Pyrogovskyi <anton@gets.love>
2024-01-31 11:08:47 +0100

    minor code refactoring/cleanup/docs

--- src/apps/mod.rs
+++ src/apps/mod.rs
@@ -1,7 +1,8 @@
 use self::generic::{GenStep, GEN_STEPS};
-use color_eyre::eyre::{Result, WrapErr};
+use color_eyre::eyre::{eyre, Result};
 use futures::{future::BoxFuture, FutureExt};
 use std::collections::HashMap;
+use std::env;
 use std::path::PathBuf;
 use thirtyfour::prelude::*;
 use url::Url;
@@ -68,6 +69,7 @@ mod web2py;
 mod wordpress;
 mod zencart;
 
+/// Custom values to use for the appliance inithook variables
 pub struct Preseeds {
     pub root_pass: String,
     pub db_pass: String,
@@ -76,6 +78,26 @@ pub struct Preseeds {
     pub app_domain: String,
 }
 
+/// Default values for the inithook variables, overridable with envvars
+impl Default for Preseeds {
+    fn default() -> Self {
+        Self {
+            root_pass: env::var("ROOT_PASS").unwrap_or("turnkey".to_owned()),
+            db_pass: env::var("DB_PASS").unwrap_or("turnkey".to_owned()),
+            app_pass: env::var("APP_PASS").unwrap_or("turnkey".to_owned()),
+            app_email: env::var("APP_EMAIL").unwrap_or("admin@example.com".to_owned()),
+            app_domain: env::var("APP_DOMAIN").unwrap_or("example.com".to_owned()),
+        }
+    }
+}
+
+/// Action to perform on the named appliance
+pub enum Action {
+    Test,
+    Install,
+}
+
+/// Running state used by `App` `Step`s
 pub struct State {
     pub name: String,
     pub wd: WebDriver,
@@ -85,39 +107,95 @@ pub struct State {
     pub pse: Preseeds,
 }
 
-pub enum Action {
-    Test,
-    Install,
-}
-
 impl State {
-    async fn wait(&self, sel: By) -> color_eyre::Result<WebElement> {
+    /// Wait for the first selector-shaped element to appear
+    async fn wait(&self, sel: By) -> WebDriverResult<WebElement> {
         let elt = self.wd.query(sel.clone()).first().await?;
-        elt.wait_until()
-            .displayed()
-            .await
-            .wrap_err(format!("waiting for {:?} to be displayed", sel))?;
+
+        elt.wait_until().displayed().await.map_err(|e| {
+            WebDriverError::CustomError(format!("waiting for {sel:?} to be displayed: {e}"))
+        })?;
+
         Ok(elt)
     }
 
+    /// Sleep for `m` milliseconds (for use when `wait()` doesn't work)
     async fn sleep(&self, m: u64) {
         thirtyfour::support::sleep(std::time::Duration::from_millis(m)).await
     }
 
+    /// Convenient goto for path under current URL root
     async fn goto(&self, path: &str) -> WebDriverResult<()> {
         self.wd.goto(self.url.join(path)?).await
     }
 
+    /// Convenient goto for path under current URL root but with a different port
     async fn goto_port(&self, port: u16, path: &str) -> WebDriverResult<()> {
         let mut u = self.url.clone();
+
         u.set_port(Some(port))
             .map_err(|()| WebDriverError::CustomError("Failed setting port".to_string()))?;
+
         self.wd.goto(u.join(path)?).await
     }
 }
 
-type StepFn = fn(&State) -> BoxFuture<'_, color_eyre::Result<()>>;
+/// Convenience alias for the actual work future a single `Step` describes
+type StepFn = fn(&State) -> BoxFuture<'_, Result<()>>;
 
+/// Single incremental step of an `App` scenario
+#[derive(Clone)]
+pub struct Step {
+    name: &'static str,
+    desc: &'static str,
+    screenshot: &'static str,
+
+    f: StepFn,
+}
+
+impl Step {
+    pub const fn default() -> Self {
+        Self {
+            name: "dummy",
+            desc: "No description provided",
+            screenshot: "",
+            f: |_| async { Ok(()) }.boxed(),
+        }
+    }
+
+    /// Run the step while taking screenshots
+    pub async fn run(&self, st: &State) -> Result<()> {
+        let res = (self.f)(st).await;
+
+        let screenshot = if res.is_err() {
+            // browser error screenshot
+            "screenshot-error.png".to_string()
+        } else if !self.screenshot.is_empty() {
+            format!("screenshot-{}.png", self.screenshot)
+        } else {
+            format!("screenshot-{}.png", self.name)
+        };
+
+        let scr_res = st.wd.screenshot(&st.ssp.join(screenshot)).await;
+        // see if step failed
+        res?;
+        // only if it didn't, see if screenshotting it failed
+        scr_res?;
+
+        Ok(())
+    }
+}
+
+impl Default for Step {
+    fn default() -> Self {
+        Self::default()
+    }
+}
+
+/// Convenience alias for a sequence of `Step`s
+type Steps = &'static [Step];
+
+/// Single app-specific scenario with steps split by stages
 pub struct App {
     pre: Steps,
     test: Steps,
@@ -144,58 +222,11 @@ impl Default for App {
     }
 }
 
-type Steps = &'static [Step];
-
-#[derive(Clone)]
-pub struct Step {
-    name: &'static str,
-    desc: &'static str,
-    screenshot: &'static str,
-
-    f: StepFn,
-}
-
-impl Step {
-    pub const fn default() -> Self {
-        Self {
-            name: "",
-            desc: "",
-            screenshot: "",
-            f: |_| async { Ok(()) }.boxed(),
-        }
-    }
-
-    pub async fn run(&self, st: &State) -> Result<()> {
-        let res = (self.f)(st).await;
-
-        let screenshot = if res.is_err() {
-            "screenshot-error.png".to_string()
-        } else if !self.screenshot.is_empty() {
-            format!("screenshot-{}.png", self.screenshot)
-        } else {
-            format!("screenshot-{}.png", self.name)
-        };
-
-        let scr_res = st.wd.screenshot(&st.ssp.join(screenshot)).await;
-        // see if step failed
-        res?;
-        // only if it didn't, see if screenshotting it failed
-        scr_res?;
-
-        Ok(())
-    }
-}
-
-impl Default for Step {
-    fn default() -> Self {
-        Self::default()
-    }
-}
-
+/// The map of appliance names to appliance scenario definitions
 pub struct Runners(HashMap<&'static str, &'static App>);
 
-impl Default for Runners {
-    fn default() -> Self {
+impl Runners {
+    pub fn new() -> Runners {
         let mut h = HashMap::new();
         h.insert("asp-net-core", &asp_net_core::APP);
         h.insert("avideo", &avideo::APP);
@@ -259,28 +290,29 @@ impl Default for Runners {
         h.insert("zencart", &zencart::APP);
         Self(h)
     }
-}
 
-impl Runners {
-    async fn run_internal(&self, st: &State) -> color_eyre::Result<()> {
+    async fn try_run(&self, st: &State) -> Result<()> {
         let app = self
             .0
             .get(st.name.as_str())
-            .ok_or(color_eyre::Report::msg(format!(
-                "Unknown app: '{:?}'!",
-                st.name
-            )))?;
+            .ok_or(eyre!("Unknown app: '{:?}'!", st.name))?;
 
-        println!("The default steps to be executed are:");
+        let (act_name, act_steps) = match st.act {
+            Action::Test => ("test", app.test),
+            Action::Install => ("install", app.install),
+        };
+
+        println!("Running steps for {} (action: {})...", st.name, act_name);
+        println!("  Default steps used:");
 
         let def_steps: &[Step] = &GEN_STEPS
             .iter()
             .filter_map(|(n, s)| {
                 if app.skip.contains(n) {
-                    println!("- (skipped by this app) {:?}", n);
+                    println!("    - (skipped by this app) {:?}", n);
                     None
                 } else {
-                    println!("- {:?}", n);
+                    println!("    - {:?}", n);
                     Some(*s)
                 }
             })
@@ -288,49 +320,37 @@ impl Runners {
             .cloned()
             .collect::<Vec<Step>>();
 
-        println!("\nRunning steps for {}...", st.name);
+        let stages = &[
+            ("Default generic steps", def_steps),
+            ("Appliance 'pre' steps", app.pre),
+            (&format!("Appliance {act_name} steps"), act_steps),
+            ("Appliance 'post' steps", app.post),
+        ];
 
-        fn stage_name(i: usize) -> &'static str {
-            match i {
-                1 => "Default generic runners",
-                2 => "Appliance pre action",
-                3 => "Appliance test/install",
-                4 => "Appliance post action",
-                _ => panic!("stage > 4 encountered!"),
-            }
-        }
+        for i in 0..stages.len() {
+            let (name, steps) = stages[i];
 
-        for (i, steps) in [
-            def_steps,
-            app.pre,
-            (match st.act {
-                Action::Test => app.test,
-                Action::Install => app.install,
-            }),
-            app.post,
-        ]
-        .iter()
-        .enumerate()
-        .map(|(n, s)| (n + 1, s))
-        {
             if steps.is_empty() {
-                println!(
-                    "  Skipping stage {} ({}): no steps defined for it",
-                    i,
-                    stage_name(i)
-                );
+                println!("  Skipping stage {i} ({name}): no steps defined for it");
                 continue;
             } else {
-                println!("  Running stage {} ({}) steps:", i, stage_name(i),);
+                println!("  Running stage {i} ({name}) steps:");
             }
 
-            if i > 1 {
+            if i > 0 {
                 // always start non-empty non-default step sequence at root url for convenience
                 st.goto("/").await?;
             }
 
-            for (j, step) in steps.iter().enumerate().map(|(n, s)| (n + 1, s)) {
-                println!("    Running step {}.{}: {} {}", i, j, step.name, step.desc);
+            for j in 0..steps.len() {
+                let step = &steps[j];
+                println!(
+                    "    Running step {}.{}: {} {}",
+                    i,
+                    j + 1,
+                    step.name,
+                    step.desc
+                );
                 step.run(st).await?
             }
         }
@@ -338,8 +358,9 @@ impl Runners {
         Ok(())
     }
 
-    pub async fn run(&self, st: State) -> color_eyre::Result<()> {
-        let o = self.run_internal(&st).await;
+    /// Wrapper function to always quit WebDriver cleanly
+    pub async fn run(&self, st: State) -> Result<()> {
+        let o = self.try_run(&st).await;
         let _ = st.wd.clone().quit().await;
         o
     }
