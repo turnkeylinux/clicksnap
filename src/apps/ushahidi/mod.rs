58ad8ef52a4c33a1e56ecfe07cd1f6302a8cb35f
Anton Pyrogovskyi <anton@gets.love>
2024-03-27 18:20:20 +0100

    add ushahidi

---
+++ src/apps/ushahidi/mod.rs
@@ -0,0 +1,49 @@
+use super::{App, State, Step};
+use futures::FutureExt;
+use thirtyfour::prelude::*;
+
+pub const APP: App = App {
+    test: &[
+        Step {
+            name: "admin-login",
+            f: |st: &State| {
+                async {
+                    st.wait(By::ClassName("onboarding-step__close-btn"))
+                        .await?
+                        .click()
+                        .await?;
+                    st.wait(By::Id("auth")).await?.click().await?;
+                    st.wait(By::Css("input[formcontrolname=email]"))
+                        .await?
+                        .send_keys("admin@example.com")
+                        .await?;
+                    st.wait(By::Css("input[formcontrolname=password]"))
+                        .await?
+                        .send_keys(&st.pse.app_pass)
+                        .await?;
+                    st.sleep(1000).await;
+                    Ok(())
+                }
+                .boxed()
+            },
+            ..Step::default()
+        },
+        Step {
+            name: "admin-dash",
+            f: |st: &State| {
+                async {
+                    st.wd
+                        .form(By::ClassName("auth-form"))
+                        .await?
+                        .submit()
+                        .await?;
+                    st.sleep(1000).await;
+                    Ok(())
+                }
+                .boxed()
+            },
+            ..Step::default()
+        },
+    ],
+    ..App::default()
+};
